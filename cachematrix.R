f85bacdf386b45f572917626dd1aa5000bc5095f
gmbell <gbell@revolutionmessaging.com>
2015-01-25 17:19:51 -0600

    implemented assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a set of functions designed to hold a square matrix
+## and store the inverse of that matrix after its computation.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix variable and holds it in a
+## structure prepared to also contain its inverse. The matrix
+## may be defined either in the initial call, or by using the
+## $set subfunction.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) i <<- solve
+        getsolve <- function() i
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve retrives the inverse of a makeCacheMatrix either
+## by returning a previously computed inverse, or by computing
+## the inverse if one had not already been created. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getsolve()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setsolve(i)
+        i
 }
